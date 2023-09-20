28009ea24e6144417f6387102ad6b553ba890aad
guibac63 <guibac63@hotmail.fr>
2023-09-20 10:56:57 +0200

    first commit

---
+++ src/main/java/com/mercadona_app/mercadona/MercadonaApplication.java
@@ -0,0 +1,13 @@
+package com.mercadona_app.mercadona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadonaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadonaApplication.class, args);
+	}
+
+}
