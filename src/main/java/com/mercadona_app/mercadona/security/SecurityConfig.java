1afe4bfa5af0396ccfe211c5d291f16a801f7a01
guibac63 <guibac63@hotmail.fr>
2023-09-20 15:53:12 +0200

    add CustomUserDetailsService - Password encoder and Autentication manager in Security config

--- src/main/java/com/mercadona_app/mercadona/security/SecurityConfig.java
+++ src/main/java/com/mercadona_app/mercadona/security/SecurityConfig.java
@@ -3,27 +3,52 @@ package com.mercadona_app.mercadona.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-  
+
+
+  private CustomUserDetailsService userDetailsService;
+
+  @Autowired
+  public SecurityConfig(CustomUserDetailsService userDetailsService) {
+    this.userDetailsService = userDetailsService;
+  }
+
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
       http
               .csrf(csrf -> csrf.disable())
               .authorizeRequests(requests -> requests
-                      .antMatchers(HttpMethod.GET).permitAll()
+                      .antMatchers(HttpMethod.GET).authenticated()
                       .anyRequest().authenticated())
               .httpBasic(withDefaults());
     
     return http.build();
 
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(
+          AuthenticationConfiguration authenticationConfiguration) throws Exception {
+            return authenticationConfiguration.getAuthenticationManager();
+          }
+  
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 
 
